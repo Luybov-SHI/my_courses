a1025d7c4d0cf950bcc44c3f532036f06836bf36
Luybov-SHI <silubov091@gmail.com>
2024-09-23 01:09:54 +0300

    Add files via upload

---
+++ дз базовые структуры данных.py
@@ -0,0 +1,17 @@
+#1st program
+from stringprep import in_table_c5
+
+print((9**0.5)*5)
+#2nd program
+print(9.99>9.98, 1000!=1001)
+#3rd program
+print(2*2+2)
+print(2*(2+2))
+print(6==8)
+#4th program
+x=123.456
+print(int(x%7))
+
+
+
+
