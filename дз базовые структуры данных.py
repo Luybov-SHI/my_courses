135cf8781ec97b5fdd9ee3de14d95829f30cb831
Luybov-SHI <silubov091@gmail.com>
2024-09-24 10:34:01 +0300

    Add files via upload

--- дз базовые структуры данных.py
+++ дз базовые структуры данных.py
@@ -1,6 +1,4 @@
 #1st program
-from stringprep import in_table_c5
-
 print((9**0.5)*5)
 #2nd program
 print(9.99>9.98 and 1000!=1001)
@@ -13,7 +11,6 @@ x=123.456
 x=float(x*10)
 int(x)
 print(int(x%10))
-
 
 
 
