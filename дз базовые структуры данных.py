b23a7b37ce614022636950b39fcce37020c61467
Luybov-SHI <silubov091@gmail.com>
2024-09-24 10:29:40 +0300

    Update дз базовые структуры данных.py

--- дз базовые структуры данных.py
+++ дз базовые структуры данных.py
@@ -3,14 +3,17 @@ from stringprep import in_table_c5
 
 print((9**0.5)*5)
 #2nd program
-print(9.99>9.98, 1000!=1001)
+print(9.99>9.98 and 1000!=1001)
 #3rd program
 print(2*2+2)
 print(2*(2+2))
 print(6==8)
 #4th program
 x=123.456
-print(int(x%7))
+x=float(x*10)
+int(x)
+print(int(x%10))
+
 
 
 
