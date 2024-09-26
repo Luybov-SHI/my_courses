733c3c1872fa3b6b81193d3c562a7a7188519b09
Luybov-SHI <silubov091@gmail.com>
2024-09-26 10:51:18 +0300

    Add files via upload

--- module_1_5.py
+++ module_1_5.py
@@ -1,6 +1,8 @@
 immutable_var=1,2,3,"Привет"
 print (tuple(immutable_var))
 print(tuple)
+# immutable_var[1]=4     кортеж-неизменяемые данные
+# print(immutable_var)
 mutable_list=[56,55,54]
 mutable_list.append('food')
 print(mutable_list)
