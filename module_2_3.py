6d06e9537df90f412cf63854e3164eb37e45c5b2
Luybov-SHI <silubov091@gmail.com>
2024-09-30 00:52:17 +0300

    Add files via upload

--- module_2_3.py
+++ module_2_3.py
@@ -3,7 +3,7 @@ from operator import index
 my_list=[42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 index = 0
 while index < len(my_list):
-    if my_list[index]<0:
+    if my_list[index]<1:
         break
     print(my_list[index])
     index+=1
